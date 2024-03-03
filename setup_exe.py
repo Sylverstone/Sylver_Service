88e2d3f016001505e7ea494b11b9eba8292cb752
Sylverstone <sylvio0801@gmail.com>
2024-03-02 23:01:56 -0400

    ajout de la 2e icone de l'app

--- setup_exe.py
+++ setup_exe.py
@@ -1,15 +1,16 @@
 import sys
+import tkinter
 from cx_Freeze import setup, Executable
 import platform,os
 
 # Obtenez le nom du système d'exploitation
 system_name = platform.system()
-
+fen = tkinter.Tk()
 # Define the executables
 if system_name != "Windows":
-    executables = [Executable("sylver_service.py", base=None, icon=os.path.join("Image", "Logo_app.ico"))]
+    executables = [Executable("sylver_service.py", base=None, icon=os.path.join("Image", "Logo_app_2.ico"))]
 else:
-    executables = [Executable("sylver_service.py", base="Win32GUI",icon=os.path.join("Image", "Logo_app.ico"))] # le cmd ne s'ouvra pas
+    executables = [Executable("sylver_service.py", base="Win32GUI",icon=os.path.join("Image", "Logo_app_2.ico"))] # le cmd ne s'ouvra pas
 
 build_options = {
     "packages": ["pygame","os","datetime","sys","threading","keyboard","tkinter","pymysql","tkinter.filedialog","time","pyperclip"],
@@ -22,14 +23,13 @@ build_options = {
         ("Ressource","Ressource"),
         ("Resize_image.py","Resize_image.py"),
         (".env", ".env"),
-        ("desktop.ini","desktop.ini")
     ],
 }
 
 # Set up the setup function
 setup(
     name="sylver_service",
-    version='v1.3',
+    version='v1.4',
     options={"build_exe": build_options},
     author = "by Sylvio PELAGE MAXIME | Elvann JOLIVEL | Deecleane CORALIE",
     executables=executables,
