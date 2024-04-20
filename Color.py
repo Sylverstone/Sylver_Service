fd8801436fcabb054178784b1de724e732db9adf
Sylverstone <sylvio0801@gmail.com>
2024-04-20 18:31:12 -0400

    Les couleurs ont été renommez et les doublons retirez. C'est plus clair mtn

--- Color.py
+++ Color.py
@@ -2,20 +2,11 @@ class Color:
     """
         Class repertoriant toute les couleurs possible dans l'applications
     """
-    def __init__(self):
-        self.fond_principal = (64,64,64) #Gris foncé
-        self.fond_bar_de_navigation = (33,33,33) #noir 
-        self.couleur_titre = (106,109,255) #melange bleu violet
-        self.fond_case = (40,40,40) #noir
-        self.couleur_contour_case = (106,109,255) #bleau pal
-        self.contour_contenaire_tuto = (142,142,142) #gris clair
-        self.fond_contenaire_login = (142,142,142) #gris clair
-        self.contour_input_login = (116,109,255) #bleu pal 
-        self.fond_case_login = (106,109,255) #bleau pall
-        self.couleur_texte_editer_pp = (116,108,255) #bleau pal
-        self.arriere_fond_login = (30,30,30) #noir
-        self.couleur_fond_case_tuto = (116,109,255) #bleau pal
-        self.fond_un_login = (142,142,142) #gris clair
-        self.fond_deux_login = (30,30,30) #noir
-        self.fond_contenaire_page_tuto = (142,142,142) #gris clair
-        self.bleu_pal = (158,155,255)
+    def __init__(self):        
+        self.Gris = (64,64,64)
+        self.Noir = (33,33,33)
+        self.Bleu = (106,109,255)
+        self.Noir_clair = (40,40,40)
+        self.Gris_clair = (142,142,142)
+        self.Bleu_clair = (158,155,255)
+        
